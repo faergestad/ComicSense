f37d8e2f6c6f9dbe261574b03de7b7c8155a0a37
faergestad <georg.f.persen@gmail.com>
2018-11-24 22:09:59 +0100

    implemented my own onScrollListener & fixed search to work with this, added ability to slide each comic to reveal options like 'favorite', 'explain' and 'share'

--- app/src/main/java/com/example/rouge/comicsense/Adapter/ComicAdapter.java
+++ app/src/main/java/com/example/rouge/comicsense/Adapter/ComicAdapter.java
@@ -1,14 +1,18 @@
 package com.example.rouge.comicsense.Adapter;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.daimajia.swipe.SwipeLayout;
 import com.example.rouge.comicsense.Model.Comic;
 import com.example.rouge.comicsense.R;
 
@@ -16,7 +20,7 @@ import java.util.List;
 
 public class ComicAdapter extends RecyclerView.Adapter<ComicAdapter.ViewHolder> {
 
-    List<Comic> comics;
+    private List<Comic> comics;
     private Context context;
 
     public ComicAdapter(Context context, List<Comic> comics) {
@@ -26,18 +30,75 @@ public class ComicAdapter extends RecyclerView.Adapter<ComicAdapter.ViewHolder> 
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int i) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_comic, parent, false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        return viewHolder;
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(ComicAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull final ComicAdapter.ViewHolder holder, int position) {
 
-        Comic comic = comics.get(position);
+        final Comic comic = comics.get(position);
 
-        String comicNr = "#" + comic.getNum();
+        holder.swipeLayout.addDrag(SwipeLayout.DragEdge.Left, holder.swipeLayout.findViewById(R.id.bottom_wrapper1));
+        holder.swipeLayout.addDrag(SwipeLayout.DragEdge.Right, holder.swipeLayout.findViewById(R.id.bottom_wrapper));
+        holder.swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+
+            }
+        });
+
+        holder.btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Added #" +
+                        comic.getNum() + " to favorites", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Clicked on Share " +
+                        holder.titleTxtView.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        holder.edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "Clicked on Edit  " +
+                        holder.titleTxtView.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        final String comicNr = "#" + comic.getNum();
 
         Glide.with(context).load(comic.getImg()).into(holder.comicImage);
         holder.titleTxtView.setText(comic.getTitle());
@@ -53,11 +114,18 @@ public class ComicAdapter extends RecyclerView.Adapter<ComicAdapter.ViewHolder> 
 
     class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        public ImageView comicImage;
-        public TextView titleTxtView, descTxtView, nrTxtView;
+        SwipeLayout swipeLayout;
+        ImageButton btnLocation;
+        TextView titleTxtView, descTxtView, nrTxtView, edit, share;
+        ImageView comicImage;
 
-        public ViewHolder(View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
+
+            swipeLayout = itemView.findViewById(R.id.swipe);
+            edit = itemView.findViewById(R.id.edit);
+            share = itemView.findViewById(R.id.share);
+            btnLocation = itemView.findViewById(R.id.btnLocation);
 
             comicImage = itemView.findViewById(R.id.main_img);
             titleTxtView = itemView.findViewById(R.id.main_title);
@@ -67,7 +135,6 @@ public class ComicAdapter extends RecyclerView.Adapter<ComicAdapter.ViewHolder> 
 
         @Override
         public void onClick(View v) {
-            // TODO add onclick on comics
         }
     }
 }
--- app/src/main/java/com/example/rouge/comicsense/Fragment/ComicFragment.java
+++ app/src/main/java/com/example/rouge/comicsense/Fragment/ComicFragment.java
@@ -1,6 +1,7 @@
 package com.example.rouge.comicsense.Fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.LinearLayoutManager;
@@ -29,17 +30,22 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ComicFragment extends Fragment implements SearchView.OnQueryTextListener{
+public class ComicFragment extends Fragment implements SearchView.OnQueryTextListener {
 
     private final String TAG = "ComicFragment";
     private RequestQueue requestQueue;
 
+    private List<Comic> allComics;
     private List<Comic> comicList;
     private List<Comic> result;
     private List<Comic> reset;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
     private LinearLayoutManager linearLayoutManager;
+    private boolean loading;
+    private int visibleItemCount, totalItemCount, pastVisibleItems, nr, newestNr, counter;
+    private final int visibleLimit = 5;
+    private final int NEWEST_COMICNR = 2076;
 
     public ComicFragment() {
     }
@@ -51,12 +57,16 @@ public class ComicFragment extends Fragment implements SearchView.OnQueryTextLis
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_comic, container, false);
 
         recyclerView = view.findViewById(R.id.recyclerview);
 
         comicList = new ArrayList<>();
+
+        allComics = new ArrayList<>();
+
         adapter = new ComicAdapter(getContext(), comicList);
 
         requestQueue = Volley.newRequestQueue(getActivity());
@@ -67,56 +77,130 @@ public class ComicFragment extends Fragment implements SearchView.OnQueryTextLis
 
         reset = new ArrayList<>();
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setAdapter(adapter);
 
-        String startUrl = "https://xkcd.com/";
-        String endUrl = "/info.0.json";
-        int nr = 2075;
+        loading = true;
 
-        while(nr > 0) {
-            getComics(startUrl + nr + endUrl);
-            nr--;
-        }
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+
+                if (dy > 0) {
+                    visibleItemCount = linearLayoutManager.getChildCount();
+                    totalItemCount = linearLayoutManager.getItemCount();
+                    pastVisibleItems = linearLayoutManager.findFirstVisibleItemPosition();
+
+                    if ((visibleItemCount + pastVisibleItems) >= totalItemCount) {
+                        Log.d("Loading:", "reached last item. It was: " + nr);
+
+                        getComics();
+                    }
+                }
+
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+        // TODO get newest comic nr from SharedPreferences
+        nr = NEWEST_COMICNR;
+        newestNr = nr;
+
+        getComics();
+        getAllComics();
 
         return view;
     }
 
-    public void getComics(String s) {
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                (Request.Method.GET, s, null, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
+    private void getAllComics() {
+        int countDown = NEWEST_COMICNR;
+        while (countDown > 0) {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                    (Request.Method.GET, "https://xkcd.com/" + countDown + "/info.0.json", null, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
 
-                        Comic comic = new Comic();
-                        comic.setMonth(response.optString("month"));
-                        comic.setNum(response.optInt("num"));
-                        comic.setLink(response.optString("link"));
-                        comic.setLink(response.optString("year"));
-                        comic.setNews(response.optString("news"));
-                        comic.setSafeTitle(response.optString("safe_title"));
-                        comic.setTranscript(response.optString("transcript"));
-                        comic.setAlt(response.optString("alt"));
-                        comic.setImg(response.optString("img"));
-                        comic.setTitle(response.optString("title"));
-                        comic.setDay(response.optString("day"));
+                            Comic comic = new Comic();
+                            comic.setMonth(response.optString("month"));
+                            comic.setNum(response.optInt("num"));
+                            comic.setLink(response.optString("link"));
+                            comic.setLink(response.optString("year"));
+                            comic.setNews(response.optString("news"));
+                            comic.setSafeTitle(response.optString("safe_title"));
+                            comic.setTranscript(response.optString("transcript"));
+                            comic.setAlt(response.optString("alt"));
+                            comic.setImg(response.optString("img"));
+                            comic.setTitle(response.optString("title"));
+                            comic.setDay(response.optString("day"));
 
-                        comicList.add(comic);
-                        reset.add(comic);
+                            allComics.add(comic);
+                            reset.add(comic);
 
-                        adapter.notifyDataSetChanged();
-                    }
-                }, new Response.ErrorListener() {
+                            //adapter.notifyDataSetChanged();
+                        }
+                    }, new Response.ErrorListener() {
 
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("volley", error.toString());
-                    }
-                });
-        requestQueue.add(jsonObjectRequest);
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("volley", error.toString());
+                        }
+                    });
+            requestQueue.add(jsonObjectRequest);
+
+            countDown--;
+        }
+    }
+
+    public void getComics() {
+        nr = NEWEST_COMICNR;
+
+        while (counter < visibleLimit) {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                    (Request.Method.GET, "https://xkcd.com/" + nr + "/info.0.json", null, new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+
+                            Comic comic = new Comic();
+                            comic.setMonth(response.optString("month"));
+                            comic.setNum(response.optInt("num"));
+                            comic.setLink(response.optString("link"));
+                            comic.setLink(response.optString("year"));
+                            comic.setNews(response.optString("news"));
+                            comic.setSafeTitle(response.optString("safe_title"));
+                            comic.setTranscript(response.optString("transcript"));
+                            comic.setAlt(response.optString("alt"));
+                            comic.setImg(response.optString("img"));
+                            comic.setTitle(response.optString("title"));
+                            comic.setDay(response.optString("day"));
+
+                            comicList.add(comic);
+                            //reset.add(comic);
+
+                            adapter.notifyDataSetChanged();
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("volley", error.toString());
+                        }
+                    });
+            requestQueue.add(jsonObjectRequest);
+
+            nr--;
+            Log.d("Loading", "nr " + nr);
+            counter++;
+            Log.d("Loading", "counter " + counter);
+        }
+        counter = 0;
+        Log.d("Loading", "counter after loop " + counter);
     }
 
     @Override
@@ -125,20 +209,15 @@ public class ComicFragment extends Fragment implements SearchView.OnQueryTextLis
         final MenuItem searchItem = menu.findItem(R.id.action_search);
         final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
         searchView.setOnQueryTextListener(this);
-        super.onCreateOptionsMenu(menu,inflater);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
-    public boolean onQueryTextSubmit(String s) {
-        // Could do something with this, but I like the search to evolve as I'm typing
-        return false;
-    }
+    public boolean onQueryTextSubmit(String query) {
 
-    @Override
-    public boolean onQueryTextChange(String query) {
         result = new ArrayList<>();
-
-        for(Comic comic : reset)
+        // TODO fix this to not only search the loaded items
+        for (Comic comic : reset)
             if (comic.getTitle().toLowerCase().contains(query) || String.valueOf(comic.getNum()).equals(query)) {
                 result.add(comic);
             }
@@ -146,10 +225,35 @@ public class ComicFragment extends Fragment implements SearchView.OnQueryTextLis
         comicList.clear();
         comicList.addAll(result);
 
-        if(query.equals("")) {
+        if (query.equals("")) {
             comicList.addAll(reset);
         }
         adapter.notifyDataSetChanged();
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+        /*result = new ArrayList<>();
+        // TODO fix this to not only search the loaded items
+        for (Comic comic : reset)
+            if (comic.getTitle().toLowerCase().contains(query) || String.valueOf(comic.getNum()).equals(query)) {
+                result.add(comic);
+            }
+
+        comicList.clear();
+        comicList.addAll(result);
+
+        if (query.equals("")) {
+            comicList.addAll(reset);
+        }
+        adapter.notifyDataSetChanged();*/
+        if(query.equals("")) {
+            comicList.clear();
+            adapter.notifyDataSetChanged();
+            getComics();
+        }
 
         return false;
     }
